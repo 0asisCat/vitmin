7275c4e19a5419d990ba6dcd99472644f4203b77
B!anca <155007644+0asisCat@users.noreply.github.com>
2024-11-04 22:50:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const allPartsBtn = document.getElementById("all-parts");
+const generalBtn = document.getElementById("general");
+const skinBtn = document.getElementById("skin");
+const hairFingerBtn = document.getElementById("hair-fingernails");
+const mouthBtn = document.getElementById("mouth");
+const abdomenBtn = document.getElementById("abdomen");
+const respiratoryBtn = document.getElementById("respiratory");
+const cardiovascularBtn = document.getElementById("cardiovascular");
+const bloodBtn = document.getElementById("blood");
+const bonesMuscleJointsBtn = document.getElementById("bones-muscle-joints");
+const nervousBtn = document.getElementById("nervous");
+
+
 
